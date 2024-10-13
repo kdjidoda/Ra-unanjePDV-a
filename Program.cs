2b90b526251c866a91d650c2a1407e986e5bbda9
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-10-13 20:37:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace računanjePDV_a
+{ 
+  class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Unesite cijenu bez PDV-a: ");
+        double cijenaBezPDV = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Unesite stopu PDV-a (%): ");
+        double stopaPDV = Convert.ToDouble(Console.ReadLine());
+
+        double iznosPDV = cijenaBezPDV * (stopaPDV / 100);
+        double cijenaSaPDV = cijenaBezPDV + iznosPDV;
+
+        Console.WriteLine("Iznos PDV-a: " + iznosPDV);
+        Console.WriteLine("Cijena sa PDV-om: " + cijenaSaPDV);
+        Console.ReadKey();
+    }
+}
+}
